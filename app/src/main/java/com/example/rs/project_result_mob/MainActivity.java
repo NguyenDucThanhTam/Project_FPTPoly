ff934020eea54aff80450f9ee3970c4e04b94f84
nguyenducthanhtam <nguyenducthanhtam1@gmail.com>
2017-12-02 14:13:21 +0700

    edit City Safety Activity

--- app/src/main/java/com/example/rs/project_result_mob/MainActivity.java
+++ app/src/main/java/com/example/rs/project_result_mob/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.rs.project_result_mob;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  static int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+
+
+
 //        blurBackground();
     }
 
