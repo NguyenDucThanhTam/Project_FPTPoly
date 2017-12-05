9a70adff24c8d3156df0ecab0e1675335b38bc59
DESKTOP-5DSRC3I\project1 <project1.hcmct@gmail.com>
2017-12-05 15:36:42 +0700

    ADD PROFILE FRAGMENT

---
+++ app/src/main/java/com/example/rs/project_result_mob/Adapter.java
@@ -0,0 +1,12 @@
+package com.example.rs.project_result_mob;
+
+/**
+ * Created by Iris Sun on 12/2/2017.
+ */
+
+
+
+
+
+
+
--- app/src/main/java/com/example/rs/project_result_mob/MainActivity.java
+++ app/src/main/java/com/example/rs/project_result_mob/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.rs.project_result_mob;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.RelativeLayout;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,7 +17,13 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
 //        blurBackground();
-    }
+        //ket noi vs firebase
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("message");
+
+            myRef.setValue("");
+        }
+
 
     private void blurBackground() {
         RelativeLayout  layout = (RelativeLayout)findViewById(R.id.layout_first);
